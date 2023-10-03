859ae5da40e680348653dced6f0131b54aa4eefc
SmailFun <nikita.shchablev2000@gmail.com>
2023-10-03 21:19:10 +0300

    Создание проекта и объявление массива строк.

---
+++ exam.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] originalArray = { "apple", "banana", "cherry", "date", "elderberry", "fig" };
+        string[] resultArray = new string[originalArray.Length];
+        int resultCount = 0;
+    }
+}
